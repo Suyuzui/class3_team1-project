89c8e7c84519c5550e0a820a8472a64bc9bf6d64
s1F10210372 <85667177+s1F10210372@users.noreply.github.com>
2021-12-10 00:04:48 +0900

    Update urls.py

--- cs3team1/urls.py
+++ cs3team1/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
+    path('ingredients', views.ingredient, name='ingredient'),
+    path('menu', views.menu, name='menu'),
 ]
