8248fc1919a552fada9ebced147ec0a5b301b7b7
s1F10210372 <85667177+s1F10210372@users.noreply.github.com>
2021-12-11 14:30:21 +0900

    inclodeを追加

--- config/urls.py
+++ config/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from cs3team1 import views as cs3team1_views
 
 urlpatterns = [
