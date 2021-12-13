bca36bd47ab9edb7137a746f7db1239146d17c4d
1F10210123 <85666828+1F10210123@users.noreply.github.com>
2021-12-13 21:14:35 +0900

    Update urls.py

--- cs3team1/urls.py
+++ cs3team1/urls.py
@@ -5,7 +5,9 @@ urlpatterns = [
     path('', views.home, name='home'),
     #path('', views.ingredient, name='ingredient'),
     path('ingredients', views.ingredient, name='ingredient'),
-    path('menu', views.menu, name='menu'),
+    #path('menu', views.menu, name='menu'),
     path('recommendation/<int:ingredients_id>', views.recommendation, name='recommendation'),
     path('recipe', views.recipe, name='recipe'),
+    path('menu', views.IndexView.as_view(), name='index'),
+    path('menu/post/<int:pk>', views.PostDetailView.as_view(), name='post_detail')
 ]
