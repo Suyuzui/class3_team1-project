0d5032a5df4bf0d5588f20877af1f1d4a60d4d0f
Suyuzui <83820092+Suyuzui@users.noreply.github.com>
2021-12-14 01:40:31 +0900

    ingredientsの編集
    TABを追加しました

--- cs3team1/static/blog/js/ingredients.js
+++ cs3team1/static/blog/js/ingredients.js
@@ -2,14 +2,14 @@
 function GethashID (hashIDName){
     if(hashIDName){
       //タブ設定
-      $('.tab li').find('a').each(function() { //タブ内のaタグ全てを取得
+      $('.categories li').find('a').each(function() { //タブ内のaタグ全てを取得
         let idName = $(this).attr('href'); //タブ内のaタグのリンク名（例）#lunchの値を取得 
         if(idName == hashIDName){ //リンク元の指定されたURLのハッシュタグ（例）http://example.com/#lunch←この#の値とタブ内のリンク名（例）#lunchが同じかをチェック
           let parentElm = $(this).parent(); //タブ内のaタグの親要素（li）を取得
-          $('.tab li').removeClass("active"); //タブ内のliについているactiveクラスを取り除き
+          $('.categories li').removeClass("active"); //タブ内のliについているactiveクラスを取り除き
           $(parentElm).addClass("active"); //リンク元の指定されたURLのハッシュタグとタブ内のリンク名が同じであれば、liにactiveクラスを追加
           //表示させるエリア設定
-          $(".area").removeClass("is-active"); //もともとついているis-activeクラスを取り除き
+          $(".vegetables").removeClass("is-active"); //もともとついているis-activeクラスを取り除き
           $(hashIDName).addClass("is-active"); //表示させたいエリアのタブリンク名をクリックしたら、表示エリアにis-activeクラスを追加 
         }
       });
@@ -17,7 +17,7 @@ function GethashID (hashIDName){
   }
   
   //タブをクリックしたら
-  $('.tab a').on('click', function() {
+  $('.categories a').on('click', function() {
     let idName = $(this).attr('href'); //タブ内のリンク名を取得  
     GethashID (idName);//設定したタブの読み込みと
     return false;//aタグを無効にする
@@ -26,8 +26,8 @@ function GethashID (hashIDName){
   
   // 上記の動きをページが読み込まれたらすぐに動かす
   $(window).on('load', function () {
-      $('.tab li:first-of-type').addClass("active"); //最初のliにactiveクラスを追加
-      $('.area:first-of-type').addClass("is-active"); //最初の.areaにis-activeクラスを追加
+      $('.categories li:first-of-type').addClass("active"); //最初のliにactiveクラスを追加
+      $('.vegetables:first-of-type').addClass("is-active"); //最初の.areaにis-activeクラスを追加
     let hashName = location.hash; //リンク元の指定されたURLのハッシュタグを取得
     GethashID (hashName);//設定したタブの読み込み
   });
