c4844e52f8f5aefcae7391ec8b0bc405c4b29c61
1F10210123 <85666828+1F10210123@users.noreply.github.com>
2021-12-13 22:22:04 +0900

    変更

--- cs3team1/views.py
+++ cs3team1/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django .views.generic import View
+from django.views.generic import View
 from .models import Post
 # Create your views here.
 def home(request):
