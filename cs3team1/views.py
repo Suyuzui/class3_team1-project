2eb9255cc390a55b6c809a0003b10771ffa87a60
1F10210123 <85666828+1F10210123@users.noreply.github.com>
2021-12-13 22:29:40 +0900

    Update views.py

--- cs3team1/views.py
+++ cs3team1/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import View
-from .models import Post
+from cs3team1.models import Post
 # Create your views here.
 def home(request):
 	return render(request, 'cs3team1/home.html')
