b6c173c9b41517fbde905dcc0fd8609a077408bd
1F10210123 <85666828+1F10210123@users.noreply.github.com>
2021-12-13 16:06:23 +0900

    Update models.py

--- cs3team1/models.py
+++ cs3team1/models.py
@@ -9,7 +9,7 @@ from django.conf import settings
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField("タイトル", max_length=200)
-    context = models.TextField("本文")
+    content = models.TextField("本文")
     created = models.DateTimeField("作成日",default=timezone.now)
 
     def __str__(self):
