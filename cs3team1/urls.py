2599d4297a141c5c6035474fd277bef12f663139
1F10210123 <85666828+1F10210123@users.noreply.github.com>
2021-12-19 22:57:57 +0900

    編集、削除機能
    献立表をHome画面に作成しました。
    投稿の詳細画面から編集、削除を行うことが出来ます。

--- cs3team1/urls.py
+++ cs3team1/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     #path('menu', views.menu, name='menu'),
     path('recommendation/<int:ingredients_id>', views.recommendation, name='recommendation'),
     #path('recipe/<int:recipe_id>', views.recipe, name='recipe'),
-    #path('menu', views.IndexView.as_view(), name='index'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
     path('post/new/', views.CreatePostView.as_view(), name='post_new'),
     path('post/<int:pk>/edit/', views.PostEditView.as_view(), name='post_edit'),
