2dfdb587a79cf0a69bace907c3e1d7b5c492de67
Suyuzui <83820092+Suyuzui@users.noreply.github.com>
2022-01-02 23:49:51 +0900

    ingredients編集
    タブを作成中

--- config/settings.py
+++ config/settings.py
@@ -62,7 +62,10 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, "templates"),
+            os.path.join(BASE_DIR, "static"),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
