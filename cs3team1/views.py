3f2d9cda6396806a29aac0da218dee0296f3de01
s1f102102773 <s1f102102773@iniad.org>
2022-01-07 11:59:02 +0900

    デモ

--- cs3team1/views.py
+++ cs3team1/views.py
@@ -19,19 +19,21 @@ def ingredient(request):
 
 def recommendation(request):
 	context = {
-		"recipes": [
-			{
-				"id": 1,
-				"title": "レシピ1",
-				"body": "sample"
-			}
-		]
+		"id": 1,
+		"title": "白菜と豚肉のレモン炒め",
+		"body": "sample"
 	}
 	return render(request, 'cs3team1/recommendation.html', context)
 
-def recipe(request, recipe_id):
+def recipe(request):
 	context = {
-		"recipe_id": recipe_id
+		"id": 1,
+		"title": "白菜と豚肉のレモン炒め",
+		"ingredients": ["白菜:中4枚", "豚ロース:薄切り100g", "長ねぎ:40g", "赤パプリカ:30g"],
+		"seasonings": ["サラダ油:小さじ1", "レモン汁:1/2個分", "にんにくすりおろし:小さじ1", "塩:小さじ1/3", "粗びき黒こしょう:少々", "ごま油:小さじ1"],
+		"howto": ["白菜は縦に4等分にして3cmの長さに切る。豚肉はこま切りに、長ねぎはみじん切り、パプリカは千切りに切っておく。", "【調味料】の材料をよく混ぜ合わせておく。", "フライパンにサラダ油を入れて熱し、中火で豚肉を炒める。火が通ったら白菜を入れて炒め合わせる。", "白菜がしんなりしてきたら、2の【調味料】を回しかけ、みじん切りにした長ねぎを入れて炒める。", "長ねぎに火が通ったら、最後にパプリカを入れて軽く混ぜ合わせ、器に盛り付ける。"],
+		"memo": "白菜はくせが無く、おひたしや漬物、鍋の食材など調理方法もさまざまです。今回は減塩になるようレモン汁を使ってさっぱりと仕上げてみました。95％が水分で栄養価は高くないのですが、ビタミンC、カリウムなどが含まれていて利尿作用や美肌効果があります。",
+		"nutrition_facts_label": ["カロリー:110", "タンパク質:5.6", "脂質:7.9", "塩分:0.5"]
 	}
 	return render(request, 'cs3team1/recipe.html', context)
 
